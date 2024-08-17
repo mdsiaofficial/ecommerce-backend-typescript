33a400601d2117e81a9c6f1fb14a11b51c3cc158
mdsiaofficial <mdsiaofficial@gmail.com>
2024-08-17 16:08:29 +0600

    "Updated orders controllers and routes: modified createOrder function, added findOrder function, and updated routes to include findOrder endpoint"

--- src/controllers/orders.controllers.ts
+++ src/controllers/orders.controllers.ts
@@ -16,11 +16,9 @@ export const createOrder = async (req:Request, res: Response, next:NextFunction)
   try {
     const { orderNumber, userId, products, totalQuantity, totalAmount, status } = req.body
 
-    if (!orderNumber || !userId || !products || !totalQuantity || !totalAmount || !status) {
+    if (!orderNumber && !userId && !products && !totalQuantity && !totalAmount && !status) {
       throw new Error("All fields are required.")
     }
-
-    
 
     const order: TOrder = await Orders.create({
       orderNumber,
@@ -32,6 +30,22 @@ export const createOrder = async (req:Request, res: Response, next:NextFunction)
     })
 
     res.status(201).json({message:"Order Created successfully", order:order})
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+export const findOrder = async (req:Request, res: Response, next:NextFunction) => {
+  try {
+    const { id } = req.params
+    const order: TOrder | null = await Orders.findById(id)
+
+    if (!order) {
+      throw new Error("Order not found.")
+    }
+
+    res.status(201).json({message:"Order successfully done", order:order})
   } catch (error) {
     next(error)
   }
--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createOrder, fetchAllOrders } from '../controllers/orders.controllers'
+import { createOrder, fetchAllOrders, findOrder } from '../controllers/orders.controllers'
 
 const ordersRouter = express.Router()
 
 ordersRouter.get("/", fetchAllOrders)
 ordersRouter.post("/", createOrder)
-// ordersRouter.get("/login", )
+ordersRouter.get("/:id", findOrder)
 
 
 export default ordersRouter
